104d47adb542625a7fb700fb6c95ec0f6f8b7c2a
CrazyLab1001 <cramerkj@mail.uc.edu>
2025-04-12 18:36:57 -0400

    Troubleshooting DataSaver + Added SafeInput

--- src/DataSaver.java
+++ src/DataSaver.java
@@ -1,11 +1,9 @@
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import static java.nio.file.StandardOpenOption.CREATE;
 
@@ -13,13 +11,33 @@ public class DataSaver {
     public static void main(String[] args) {
         ArrayList <String> toRecord = new ArrayList<>();
         String stringDataInput;
+        int altDataInput;
+        Scanner in = new Scanner(System.in);
+        stringDataInput = SafeInput.getNonZeroLenString(in,"What's the first name?");
+        toRecord.add(stringDataInput + ",");
+        stringDataInput = SafeInput.getNonZeroLenString(in,"What's the last name?");
+        toRecord.add(stringDataInput + ",");
+        altDataInput = SafeInput.getRangedInt(in,"What's the ID number?",0,999999);
+        String formattedID = String.format("%06d", altDataInput);
+        toRecord.add(formattedID + ",");
+        stringDataInput = SafeInput.getNonZeroLenString(in,"What's the email?");
+        toRecord.add(stringDataInput + ",");
+        altDataInput = SafeInput.getRangedInt(in,"What's the year of birth?", 1000, 9999);
+        toRecord.add(altDataInput + ",");
         // prompt for data inputs of Name, ID, email, and YOB. They'll be compiled together in the ArrayList "toRecord"
+
         File workingDirectory = new File(System.getProperty("user.dir"));
         Path file = Paths.get(workingDirectory.getPath() + "\\src\\data.txt"); // creates/overrides data.txt in
         try{
             OutputStream out = new BufferedOutputStream(Files.newOutputStream(file, CREATE));
-            // Bufferedwriter code here
-            // for loop to write each line here. toRecord is recorded (lol) by each line; Six inputs.
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out));
+
+            for (String rec : toRecord) {
+                writer.write(rec,0, rec.length());
+                writer.newLine();
+            } // writes line by line
+            writer.close();
+
             System.out.println("File written! Closing program. :)");
         } catch (IOException e) {
             e.printStackTrace();
